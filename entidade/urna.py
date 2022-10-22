da699957b11d1174fe3885e68c2079a8da3dfe0e
Mateus Pereira <mateus.sle.per@hotmail.com>
2022-10-22 17:05:47 -0300

    Adicionado controlador e entidade Urna

---
+++ entidade/urna.py
@@ -0,0 +1,35 @@
+class Urna:
+    def __init__(self, reitores: list, pro_reitores: list, eleitores: list):
+        self.__candidatos = [*reitores, *pro_reitores]
+        self.__eleitores = eleitores
+        self.__homologada = False
+        self.__votos = []
+        self.__turno = 1
+
+    @property
+    def candidatos(self) -> list:
+        return self.__candidatos
+
+    @property
+    def eleitores(self) -> list:
+        return self.__eleitores
+
+    @property
+    def homologada(self) -> bool:
+        return self.__homologada
+
+    @homologada.setter
+    def homologada(self, homologada):
+        self.__homologada = homologada
+
+    @property
+    def votos(self) -> list:
+        return self.__votos
+
+    @property
+    def turno(self) -> int:
+        return self.__turno
+
+    @turno.setter
+    def turno(self, turno):
+        self.__turno = turno
